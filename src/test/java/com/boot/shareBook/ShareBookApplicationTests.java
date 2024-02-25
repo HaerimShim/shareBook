e28efb5aa9bf359ea34308f8f7e0b80be508b5e5
hrming <hae913131@gmail.com>
2024-02-26 06:17:10 +0900

    git test

---
+++ src/test/java/com/boot/shareBook/ShareBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.shareBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
