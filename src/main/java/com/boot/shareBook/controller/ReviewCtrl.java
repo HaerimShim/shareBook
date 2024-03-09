1e73a19bf5ad526b480afaf799123281c49e0347
hrming <hae913131@gmail.com>
2024-03-09 23:56:00 +0900

    Feat: 리뷰 게시판 검색/페이징 기능 추가

--- src/main/java/com/boot/shareBook/controller/ReviewCtrl.java
+++ src/main/java/com/boot/shareBook/controller/ReviewCtrl.java
@@ -4,13 +4,14 @@ import com.boot.shareBook.model.Review;
 import com.boot.shareBook.repository.ReviewRepository;
 import com.boot.shareBook.validator.ReviewValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,8 +27,14 @@ public class ReviewCtrl {
     private ReviewValidator reviewValidator;
 
     @GetMapping("/list")
-    public String getReviewList(Model model) {
-        List<Review> reviews = reviewRepository.findAll();
+    public String getReviewList(Model model, @PageableDefault(size = 6) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Review> reviews = reviewRepository.findAll(pageable);
+        Page<Review> reviews = reviewRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, reviews.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(reviews.getTotalPages(), reviews.getPageable().getPageNumber() +4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("reviews", reviews);
         return "reviewList";
     }
@@ -37,6 +44,7 @@ public class ReviewCtrl {
         model.addAttribute("review", new Review());
         return "reviewWrite";
     }
+
 
     @PostMapping("/write")
     public String postReviewWrite(@ModelAttribute Review review, BindingResult bindingResult) {
--- src/main/java/com/boot/shareBook/repository/ReviewRepository.java
+++ src/main/java/com/boot/shareBook/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.boot.shareBook.repository;
 
 import com.boot.shareBook.model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository <Review, Long> {
 
+    Page<Review> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
